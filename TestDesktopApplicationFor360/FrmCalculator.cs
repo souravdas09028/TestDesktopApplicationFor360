096cbfc709022d3060c10668966375eb5d8888db
Sourav Das <souravdas09028@gmail.com>
2025-02-08 01:22:15 +0600

    Error Resolved

---
+++ TestDesktopApplicationFor360/FrmCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDesktopApplicationFor360
+{
+    public partial class FrmCalculator : Form
+    {
+        public FrmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int val1 = Convert.ToInt32(txtVal1.Text);
+            int val2 = Convert.ToInt32(txtVal2.Text);
+
+            int sum = val1 + val2;
+
+            MessageBox.Show(sum.ToString(), "Result", MessageBoxButtons.OK);
+        }
+    }
+}
