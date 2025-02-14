a21309232ea38386a3ded4b8d31a9c9aa6ce0e0d
pulaak98 <m.k.pulak@gmail.com>
2025-02-14 14:50:35 +0600

    Updated UI,added add,subtrct,multi,dvsn,clear buttons and their click events

--- TestDesktopApplicationFor360/FrmCalculator.Designer.cs
+++ TestDesktopApplicationFor360/FrmCalculator.Designer.cs
@@ -33,17 +33,24 @@
             txtVal1 = new TextBox();
             txtVal2 = new TextBox();
             label2 = new Label();
+            btnDivision = new Button();
+            btnSubtract = new Button();
+            btnMulti = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            resultBox = new TextBox();
+            btnClear = new Button();
             SuspendLayout();
             // 
             // btnCalculate
             // 
-            btnCalculate.Location = new Point(278, 147);
+            btnCalculate.Location = new Point(52, 179);
             btnCalculate.Name = "btnCalculate";
             btnCalculate.Size = new Size(75, 23);
             btnCalculate.TabIndex = 0;
-            btnCalculate.Text = "Calculate";
+            btnCalculate.Text = "Addition";
             btnCalculate.UseVisualStyleBackColor = true;
-            btnCalculate.Click += btnCalculate_Click;
+            btnCalculate.Click += btnAddition_Click;
             // 
             // label1
             // 
@@ -60,6 +67,7 @@
             txtVal1.Name = "txtVal1";
             txtVal1.Size = new Size(100, 23);
             txtVal1.TabIndex = 2;
+            txtVal1.Click += txtVal1_Click;
             // 
             // txtVal2
             // 
@@ -67,6 +75,7 @@
             txtVal2.Name = "txtVal2";
             txtVal2.Size = new Size(100, 23);
             txtVal2.TabIndex = 3;
+            txtVal2.Click += txtVal2_Click;
             // 
             // label2
             // 
@@ -77,14 +86,92 @@
             label2.TabIndex = 4;
             label2.Text = "Please Input Second Number :";
             // 
+            // btnDivision
+            // 
+            btnDivision.Location = new Point(365, 179);
+            btnDivision.Name = "btnDivision";
+            btnDivision.Size = new Size(75, 23);
+            btnDivision.TabIndex = 5;
+            btnDivision.Text = "Division";
+            btnDivision.UseVisualStyleBackColor = true;
+            btnDivision.Click += btnDivision_Click;
+            // 
+            // btnSubtract
+            // 
+            btnSubtract.Location = new Point(146, 179);
+            btnSubtract.Name = "btnSubtract";
+            btnSubtract.Size = new Size(90, 23);
+            btnSubtract.TabIndex = 5;
+            btnSubtract.Text = "Subtraction";
+            btnSubtract.UseVisualStyleBackColor = true;
+            btnSubtract.Click += btnSubtract_Click;
+            // 
+            // btnMulti
+            // 
+            btnMulti.Location = new Point(253, 179);
+            btnMulti.Name = "btnMulti";
+            btnMulti.Size = new Size(100, 23);
+            btnMulti.TabIndex = 5;
+            btnMulti.Text = "Multiplication";
+            btnMulti.UseVisualStyleBackColor = true;
+            btnMulti.Click += btnMulti_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(146, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Choose your Operation";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(97, 212);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 25);
+            label4.TabIndex = 1;
+            label4.Text = "Result";
+            // 
+            // resultBox
+            // 
+            resultBox.BackColor = SystemColors.Control;
+            resultBox.BorderStyle = BorderStyle.FixedSingle;
+            resultBox.Enabled = false;
+            resultBox.Font = new Font("Segoe UI", 12F);
+            resultBox.Location = new Point(194, 212);
+            resultBox.Name = "resultBox";
+            resultBox.ReadOnly = true;
+            resultBox.Size = new Size(100, 29);
+            resultBox.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(325, 214);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(88, 25);
+            btnClear.TabIndex = 6;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
             // FrmCalculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(413, 198);
+            ClientSize = new Size(485, 272);
+            Controls.Add(btnClear);
+            Controls.Add(btnMulti);
+            Controls.Add(btnSubtract);
+            Controls.Add(btnDivision);
             Controls.Add(label2);
+            Controls.Add(resultBox);
             Controls.Add(txtVal2);
             Controls.Add(txtVal1);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(label1);
             Controls.Add(btnCalculate);
             Name = "FrmCalculator";
@@ -100,5 +187,12 @@
         private TextBox txtVal1;
         private TextBox txtVal2;
         private Label label2;
+        private Button btnDivision;
+        private Button btnSubtract;
+        private Button btnMulti;
+        private Label label3;
+        private Label label4;
+        private TextBox resultBox;
+        private Button btnClear;
     }
 }
--- TestDesktopApplicationFor360/FrmCalculator.cs
+++ TestDesktopApplicationFor360/FrmCalculator.cs
@@ -10,21 +10,113 @@ using System.Windows.Forms;
 
 namespace TestDesktopApplicationFor360
 {
+
     public partial class FrmCalculator : Form
     {
+
+
         public FrmCalculator()
         {
             InitializeComponent();
+            txtVal1.Text = "0";
+            txtVal2.Text = "0";
+            resultBox.Text = "0";
+
         }
 
-        private void btnCalculate_Click(object sender, EventArgs e)
+        private void btnAddition_Click(object sender, EventArgs e)
         {
-            int val1 = Convert.ToInt32(txtVal1.Text);
-            int val2 = Convert.ToInt32(txtVal2.Text);
 
-            int sum = val1 + val2;
+            resultBox.Text = Calculation("add").ToString();
 
-            MessageBox.Show(sum.ToString(), "Result", MessageBoxButtons.OK);
+        }
+
+
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = Calculation("subtract").ToString();
+
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = Calculation("multiply").ToString();
+
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = Calculation("divide").ToString();
+
+        }
+
+        private double Calculation(string oparation)
+        {
+            double result = 0;
+            try
+            {
+
+                double num1 = Convert.ToDouble(txtVal1.Text);
+                double num2 = Convert.ToDouble(txtVal2.Text);
+
+                switch (oparation)
+                {
+                    case "add":
+                        result = num1 + num2;
+                        break;
+                    case "subtract":
+                        result = num1 - num2;
+                        break;
+                    case "multiply":
+                        result = num1 * num2;
+                        break;
+                    case "divide":
+                        result = num1 / num2;
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("can not divide by zero");
+                            return 0;
+
+                        }
+                        break;
+                    default:
+                        {
+                            MessageBox.Show("error");
+                            return 0;
+                        }
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numeric values.");
+            }
+            //catch (DivideByZeroException) {
+            //    MessageBox.Show("can not divide by zero");
+            //}
+            return result;
+        }
+
+        private void txtVal1_Click(object sender, EventArgs e)
+        {
+            txtVal1.Clear();
+        }
+
+        private void txtVal2_Click(object sender, EventArgs e)
+        {
+            txtVal2.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtVal1.Text = "0";
+            txtVal2.Text = "0";
+            resultBox.Text= "0";
         }
     }
 }
